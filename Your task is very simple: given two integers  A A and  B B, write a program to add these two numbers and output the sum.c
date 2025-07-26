1f2531bbd1de5a658373b45a646f4bb51346c026
Saikat Talukder <saikat60mu@gmail.com>
2025-07-26 23:35:23 +0600

    Update Your task is very simple: given two integers  A A and  B B, write a program to add these two numbers and output the sum.c

--- Your task is very simple: given two integers  A A and  B B, write a program to add these two numbers and output the sum.c
+++ Your task is very simple: given two integers  A A and  B B, write a program to add these two numbers and output the sum.c
@@ -2,12 +2,10 @@
 int main() {
     int T, A, B;
     scanf("%d", &T);
-
     // Loop through each test case
     for (int i = 0; i < T; i++) {
         // Read the two integers A and B
-        scanf("%d %d", &A, &B);
-      
+        scanf("%d %d", &A, &B);     
         // Output the sum of A and B
         printf("%d\n", A + B);
     }
